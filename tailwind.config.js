5281b75dc6ccc85909ebf66bb0de532dd6ce30a9
zawzawmyint <cuzawzawmyint@gmail.com>
2023-01-21 00:47:16 +0630

    [fix] tailwind not wroking

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
