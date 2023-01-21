3336ae42333555079f30191933c4c7d89f0bf487
zawzawmyint <cuzawzawmyint@gmail.com>
2023-01-21 13:17:32 +0630

    [add] footer

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,4 +1,4 @@
-import Footer from "@/src/Components/Footer";
+import Footer from "@/src/components/Footer";
 import { Header } from "@/src/components/Header";
 
 export default function Layout({ children }) {
