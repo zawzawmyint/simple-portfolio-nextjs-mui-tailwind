a2d039e0cf5f8b492786447fcae88e00557beabf
zawzawmyint <cuzawzawmyint@gmail.com>
2023-01-22 15:51:41 +0630

    [add] speedDial

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,13 @@ import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <Box className=" bg-neutral-800 rounded" sx={{ color: "white" }}>
+    <Box
+      className=" bg-gradient-to-r from-gray-700 via-gray-600 to-gray-500 rounded opacity-80"
+      sx={{ color: "white" }}
+    >
       <Box className=" text-center p-1 flex flex-col gap-2">
-        <Typography variant="h4" className=" font-mono font-bold">
-          Say <span className=" animate-pulse text-blue-700">Hello 👋</span>
+        <Typography variant="h4" className=" font-mono font-bold ">
+          Say <span className=" animate-pulse text-green-500">Hello 👋</span>
         </Typography>
         <Typography className=" font-mono text-lg">
           I am available for freelance projects, remote work or just discussing
